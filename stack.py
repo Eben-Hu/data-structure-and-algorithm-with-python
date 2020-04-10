e24e7ea1f4212991142dc3b58cf11a785a019120
Eben Hu <EbenHu@outlook.com>
2020-04-10 10:05:42 +0800

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,21 @@
+
+# here is the defination of class Stack
+class Stack:
+    def __init__(self):
+        self.items = []
+    
+    def isEmpty(self):
+        return self.items == []
+    
+    def push(self,item):
+        self.items.append(item)
+    
+    def pop(self):
+        self.items.pop()
+    
+    def peek(self):
+        return self.items[-1]
+    
+    def size(self):
+        return len(self.items)
+
