2bfd9ea664ca0a00ee6caf43408627895e1686e0
Eben Hu <EbenHu@outlook.com>
2020-05-08 21:46:56 +0800

    Create queue.py

---
+++ queue.py
@@ -0,0 +1,48 @@
+# queue: first in first out :从队尾进去，从队首出去
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items ==[]
+    
+    def enqueue(self,item):
+        self.items.insert(0,item)
+    
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+    # 热土豆/约瑟夫问题
+    # def hotPotato(namelist,num):
+    #     simqueue = Queue()
+    #     for name in namelist:
+    #         simqueue.enqueue(name)
+        
+    #     while not simqueue.isEmpty():
+    #         for i in range(num):
+    #             simqueue.enqueue(simqueue.dequeue())
+
+    #         simqueue.dequeue()
+        
+    #     return simqueue.dequeue()
+    
+    # def hotPotato(namelist, num):
+    #     simqueue = Queue()
+    #     for name in namelist:
+    #         simqueue.enqueue(name)
+
+    #     while simqueue.size() > 1:
+    #         for i in range(num):
+    #             simqueue.enqueue(simqueue.dequeue())
+
+    #         simqueue.dequeue()
+
+    #     return simqueue.dequeue()
+
+    # print(hotPotato(['1','2','3','4','5','6','7'],7))
+
+    # 打印机任务
+    
